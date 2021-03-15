f05501c0aa528cabd8592ed55462e7d8a5c05127
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-03-15 17:38:22 +0300

    Add movie detail page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Nav from "./components/Nav";
 import AboutUs from "./pages/AboutUs";
 import OurWork from "./pages/OurWork";
 import ContactUs from "./pages/ContactUs";
+import MovieDetail from "./pages/MovieDetail";
 
 // Global Styles
 import GlobalStyle from "./components/GlobalStyle";
@@ -21,8 +22,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
         <Route path="/contact">
           <ContactUs />
