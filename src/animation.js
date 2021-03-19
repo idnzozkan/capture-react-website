380f408775e6f08f781cbc6fc041d1c9415e94b5
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-03-19 19:50:10 +0300

    Add wave svg and animate it

--- src/animation.js
+++ src/animation.js
@@ -7,7 +7,9 @@ export const pageAnimation = {
     opacity: 1,
     y: 0,
     transition: {
-      duration: 1
+      duration: 1,
+      when: "beforeChildren",
+      staggerChildren: 0.25
     }
   },
   exit: {
@@ -15,6 +17,47 @@ export const pageAnimation = {
     y: 300,
     transition: {
       duration: 0.5
+    }
+  }
+};
+
+export const titleAnim = {
+  hidden: {
+    y: 200
+  },
+  show: {
+    y: 0,
+    transition: {
+      duration: 0.75,
+      ease: "easeOut"
+    }
+  }
+};
+
+export const fade = {
+  hidden: {
+    opacity: 0
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      ease: "easeOut"
+    }
+  }
+};
+
+export const photoAnim = {
+  hidden: {
+    opacity: 0,
+    scale: 1.5
+  },
+  show: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.75,
+      ease: "easeOut"
     }
   }
 };
