16d30898bc473d161e17fc5807a6c219b7cc9d73
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-03-21 14:31:32 +0300

    Add reusable toggle component for FAQ section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Toggle from "./Toggle";
 
 // Styles
 import styled from "styled-components";
@@ -10,17 +11,19 @@ const FaqSection = () => {
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit,
-            minima!
-          </p>
+      <Toggle>
+        <div className="question">
+          <h4>How Do I Start?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit,
+              minima!
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
+      </Toggle>
       <div className="question">
         <h4>Daily Schedule</h4>
         <div className="answer">
